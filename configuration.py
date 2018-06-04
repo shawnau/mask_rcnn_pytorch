dfdb6c3aef345c6eab17f3981543ae7321c117ea
xiaoxuan1123 <xxuan@mail.ustc.edu.cn>
2018-06-04 13:21:48 +0800

    update git ignore, add doc for config

--- configuration.py
+++ configuration.py
@@ -8,15 +8,15 @@ class Configuration(object):
         self.device = torch.device("cuda:0" if torch.cuda.is_available() else "cpu")
         self.version = 'SE-FPN-ResNeXt50'
         self.data_dir = os.path.join(os.path.dirname(os.path.realpath(__file__)), 'data', 'dsb2018')
-        # net
-        # number of the classes, include background class 0
+        # net ---------------------------------------------------------------
+        # number of the classes, including background class 0
         self.num_classes = 2
 
         # multi-rpn  --------------------------------------------------------
         # base size of the anchor box on input image
         self.rpn_base_sizes = [8, 16, 32, 64]
         self.rpn_scales = [2, 4, 8, 16]
-        # anchor
+        # anchor aspects. please referring to the doc
         aspect = lambda s, r: (s * 1 / r ** 0.5, s * r ** 0.5)
         self.rpn_base_aspect_ratios = [
             [(1, 1)],
@@ -25,30 +25,31 @@ class Configuration(object):
              aspect(2 ** 0.25, 0.25), ],
             [(1, 1), aspect(2 ** 0.5, 1), aspect(2 ** 0.25, 2), aspect(2 ** 0.25, 0.5), ],
         ]
-
+        # background: 0.0 < overlap < 0.5
+        # foreground: 0.5 < overlap < 1.0
         self.rpn_train_bg_thresh_high = 0.5
         self.rpn_train_fg_thresh_low  = 0.5
 
-        self.rpn_train_scale_balance = False
-
+        # select anchor boxes with
+        # score > 0.5, overlap > 0.85 , size > 5 pixel^2 as nms output for training
         self.rpn_train_nms_pre_score_threshold = 0.50
-        self.rpn_train_nms_overlap_threshold   = 0.85  # higher for more proposals for mask training
+        self.rpn_train_nms_overlap_threshold   = 0.85
         self.rpn_train_nms_min_size = 5
-
+        # same as training
         self.rpn_test_nms_pre_score_threshold = 0.60
         self.rpn_test_nms_overlap_threshold   = 0.75
         self.rpn_test_nms_min_size = 5
 
         # rcnn ------------------------------------------------------------------
-        self.rcnn_crop_size         = 14
-        self.rcnn_train_batch_size  = 32  # per image
-        self.rcnn_train_fg_fraction = 0.5
-        self.rcnn_train_fg_thresh_low  = 0.5
-        self.rcnn_train_bg_thresh_high = 0.5
-        self.rcnn_train_bg_thresh_low  = 0.0
-
+        self.rcnn_crop_size         = 14  # roi align pooling size
+        self.rcnn_train_batch_size  = 32  # rcnn proposals for training per image
+        self.rcnn_train_fg_fraction = 0.5     # foreground fraction for training
+        self.rcnn_train_fg_thresh_low  = 0.5  # 0.5 < overlap < 1.0 as foreground
+        self.rcnn_train_bg_thresh_high = 0.5  # thresh_low < overlap < thresh_high as background
+        self.rcnn_train_bg_thresh_low  = 0.0  # same as above
+        # same as rpn
         self.rcnn_train_nms_pre_score_threshold = 0.05
-        self.rcnn_train_nms_overlap_threshold   = 0.85  # high for more proposals for mask
+        self.rcnn_train_nms_overlap_threshold   = 0.85
         self.rcnn_train_nms_min_size = 8
 
         self.rcnn_test_nms_pre_score_threshold = 0.50
@@ -57,11 +58,11 @@ class Configuration(object):
 
         # mask ------------------------------------------------------------------
         self.mask_crop_size            = 14  # input of mask head
-        self.mask_train_batch_size     = 32  # per image
-        self.mask_size                 = 28  # out put of mask head
+        self.mask_train_batch_size     = 32  # mask proposals for training per image
+        self.mask_size                 = 28  # output size of mask head
         self.mask_train_min_size       = 8
         self.mask_train_fg_thresh_low  = self.rpn_train_fg_thresh_low
-
+        # same as rpn
         self.mask_test_nms_pre_score_threshold = 0.5
         self.mask_test_nms_overlap_threshold = 0.2
         self.mask_test_mask_threshold  = 0.5
